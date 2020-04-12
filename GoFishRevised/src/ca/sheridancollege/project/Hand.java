89b114d26c5959127d417310676af929befbd981
AbigaleRobson109 <59975241+AbigaleRobson109@users.noreply.github.com>
2020-04-12 13:19:47 -0400

    Update Hand.java

--- GoFishRevised/src/ca/sheridancollege/project/Hand.java
+++ GoFishRevised/src/ca/sheridancollege/project/Hand.java
@@ -15,9 +15,9 @@ import java.util.ArrayList;
  * @author abiga
  */
 public class Hand {
-    
+
     private ArrayList<Card> playersHand;
-     
+
     public Hand() {
         playersHand = new ArrayList<Card>();
     }
@@ -29,54 +29,67 @@ public class Hand {
     public void setPlayersHand(ArrayList<Card> playersHand) {
         this.playersHand = playersHand;
     }
-    
-    
+
     /**
      * create a hand for start of the game
-     * @param deck     */
-    public void buildHand(ArrayList<Card> deck) {
-        for (int i = 0; i < 7; i++) {
-            int num = (int) (Math.random() * ((deck.size() - 0 + 1) - 0));
-            //add card to players hand
-            playersHand.add(deck.get(num));
-            //remove card from deck
-            deck.remove(num);
-        }
-    }
-    
-     /**
-     * Take away card from deck and add that card to players hand
+     *
      * @param deck
      */
-    public void draw(ArrayList<Card> deck){
-        int num = (int) (Math.random() * ((deck.size() - 0 + 1) - 0));
+    public void buildHand(ArrayList<Card> deck) {
+        for (int i = 0; i < 7; i++) {
+            try {
+                int num = (int) Math.floor(Math.random() * ((deck.size() - 0 + 1) - 0));
+                //add card to players hand
+                playersHand.add(deck.get(num));
+                //remove card from deck
+                deck.remove(num);
+            } catch (Exception e) {
+                draw(deck);
+            }
+        }
+    }
+
+    /**
+     * Take away card from deck and add that card to players hand
+     *
+     * @param deck
+     */
+    public void draw(ArrayList<Card> deck) {
+        int num = (int) Math.floor(Math.random() * ((deck.size() - 0 + 1) - 0));
         playersHand.add(deck.get(num));
         deck.remove(num);
     }
-    
-     /**
+
+    /**
      * Search players hand for specified guess
+     *
      * @param guess
      * @return
      */
-    public boolean search(Card guess){
-        return playersHand.contains(guess);
+    public int search(String guess) {
+        int index = -1;
+        for (int i = 0; i < playersHand.size(); i++) {
+            if (playersHand.get(i).getValue() == Card.Value.valueOf(guess)) {
+                index = i;
+            }
+        }
+        return index;
     }
-    
-    public void drawFive(ArrayList<Card> deck){
+
+    public void drawFive(ArrayList<Card> deck) {
         for (int i = 0; i < 5; i++) {
             draw(deck);
         }
     }
-    
+
     /**
      * Method to display all cards in players hand
      */
-    public void showCards(){
+    public void showCards() {
         System.out.println("Your current hand");
 
         for (Card card : playersHand) {
-            System.out.println(card.getValue() + " of " + card.getSuit());
+            System.out.println(card.getSuit() + " " + card.getValue());
         }
     }
 }
